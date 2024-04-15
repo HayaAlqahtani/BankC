def20817893770081a4b1516984068d2ad8aba9d
HayaAlqahtani <HayaAlqahtani10@gmail.com>
2024-04-15 14:22:32 +0300

    Add project files.

---
+++ ConsoleApp1/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class BankAccount
+{
+    private string username;
+    private int accountNumber;
+    private double balance;
+    private const int maxTransaction = 50;
+    private double[] transactions = new double[maxTransaction];
+    private int transactionCount = 0;
+
+    public BankAccount(string name, int accountNumber, double initialBalance)
+    {
+        this.username = name;
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public void Deposit(double amount)
+    {
+        balance += amount;
+        transactions[transactionCount++] = amount;
+        Console.WriteLine($"Deposit successful \nNew Balance: {balance}");
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            transactions[transactionCount++] = -amount;
+            Console.WriteLine($"Withdrawal successful \nNew Balance: {balance}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds.");
+        }
+    }
+
+    public void ViewBalance()
+    {
+        Console.WriteLine($"Current Balance: {balance}");
+    }
+
+    public void ViewTransactionHistory()
+    {
+        Console.WriteLine("\nTransaction History:");
+        for (int i = 0; i < transactionCount; i++)
+        {
+            Console.WriteLine(transactions[i] > 0 ? $"Deposit: +${transactions[i]}" : $"Withdrawal: -${-transactions[i]}");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter Account number:");
+        int accountNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter balance amount:");
+        double balance = double.Parse(Console.ReadLine());
+
+        BankAccount account = new BankAccount(name, accountNumber, balance);
+
+        int choice;
+        do
+        {
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. View Balance:");
+            Console.WriteLine("4. View Transaction History");
+            Console.WriteLine("5. Exit");
+            Console.WriteLine("\nEnter your choice:");
+            choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter deposit amount: ");
+                    double depositAmount = double.Parse(Console.ReadLine());
+                    account.Deposit(depositAmount);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter withdrawal amount: ");
+                    double withdrawalAmount = double.Parse(Console.ReadLine());
+                    account.Withdraw(withdrawalAmount);
+                    break;
+                case 3:
+                    account.ViewBalance();
+                    break;
+                case 4:
+                    account.ViewTransactionHistory();
+                    break;
+                case 5:
+                    Console.WriteLine("Thank you");
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+        } while (choice != 5);
+    }
+}
